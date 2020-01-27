82164f0e9254014a0abb4b05243baf61f59bb10d
luisAsaenz <59874758+luisAsaenz@users.noreply.github.com>
2020-01-27 11:59:46 -0500

    Add files via upload

---
+++ Getting_Started.py
@@ -0,0 +1,9 @@
+import math
+
+print('The radius is')
+radius = int(input())
+
+if radius > 0:
+    circumference = 2 * radius * math.pi
+    print('The circle of the circumference is ')
+    print(circumference)
